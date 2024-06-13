28a17f3a0b6d7062c2ba2367e36e0334bba86e9a
StrJosedavi <josedavi100@gmail.com>
2024-06-13 01:43:03 -0300

    Feat: Starting project

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using WassamaraManagement.Data;
+using WassamaraManagement.DTOs.Mappers;
+using WassamaraManagement.Ioc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
+DependencyInjection.ConfigureServices(builder.Services);
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# WassamaraManagement
+# Projeto de API RESTful para Gerenciamento de Clientes
+
+## Descrição
+
+Este projeto é uma API RESTful desenvolvida em .NET 8 para o gerenciamento de clientes. A API permite realizar operações CRUD (Create, Read, Update, Delete) em clientes de dois tipos: Pessoa Física (PF) e Pessoa Jurídica (PJ). A autenticação básica é implementada para garantir o acesso seguro à API. O banco de dados utilizado é o PostgreSQL.
+
+## Requisitos
+
+- .NET 8
+- PostgreSQL
+- Ferramentas para teste de API (como Postman ou cURL)
+
+## Instalação
+
+1. **Clone o repositório**
+   ```bash
+   git clone https://github.com/seu-usuario/nome-do-repositorio.git
+   cd nome-do-repositorio
