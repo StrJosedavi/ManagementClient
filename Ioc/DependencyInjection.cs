39ffad2898e3d90a38c9bf05e3fae30111743d8f
StrJosedavi <josedavi100@gmail.com>
2024-06-13 01:47:09 -0300

    Feat: Dependency Injection layer

---
+++ Ioc/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using WassamaraManagement.Repository;
+using WassamaraManagement.Repository.Interfaces;
+using WassamaraManagement.Repository.UnitOfWork;
+using WassamaraManagement.Services;
+using WassamaraManagement.Services.Interfaces;
+
+namespace WassamaraManagement.Ioc
+{
+    public class DependencyInjection
+    {
+        public static IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IPersonRepository, PersonRepository>();
+            services.AddScoped<IPersonService, PersonService>();
+
+            return services;
+        }
+    }
+}
